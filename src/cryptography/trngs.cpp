cf87cf56fd391643ab91c776c014275d1069caff
Kallam Tarun Kumar Reddy <catchtherainbow6@protonmail.com>
2025-06-25 11:08:46 +0530

    Modified ring_oscillator_with_shifting

--- src/cryptography/trngs.cpp
+++ src/cryptography/trngs.cpp
@@ -4,25 +4,26 @@ using namespace std;
 
 class trng{
     public:
-        __uint128_t ring_oscillator_with_shifting(__uint128_t number_of_rounds, __uint128_t number_of_shift_registers, __uint128_t number_of_xor_gates)
+        __uint128_t ring_oscillator_with_shifting(__uint128_t initial_value, __uint128_t number_of_rounds, __uint128_t number_of_shift_registers, __uint128_t number_of_xor_gates)
         {
-            __uint128_t random_number = 1, xor_gate_count = 0;
+            __uint128_t xor_gate_count = 0;
             for (__uint128_t round = 0; round < number_of_rounds; round++)
-                random_number = ~random_number;
+                initial_value = ~initial_value;
             for (__uint128_t shift_register  = 0; shift_register < number_of_shift_registers; shift_register++)
             {
                 if (shift_register % 2 == 0 && xor_gate_count < number_of_xor_gates)
                 {
-                    random_number = random_number ^ 1;
-                    random_number << 1;
+                    initial_value = initial_value ^ 1;
+                    initial_value << 1;
                 }
                 else if (shift_register % 2 == 1)
                 {
-                    random_number = random_number ^ 1;
-                    random_number >> 1;
+                    initial_value = initial_value ^ 1;
+                    initial_value >> 1;
                 }
             }
 
+            __uint128_t random_number = initial_value;
             return random_number;
         }
 };
@@ -30,6 +31,7 @@ class trng{
 int main()
 {
     trng t;
-    long long int random_number = t.ring_oscillator_with_shifting(50,90,80);
-    cout << random_number << endl;
+    __uint128_t random_number = t.ring_oscillator_with_shifting(0x123456789,50,90,80);
+    printf("%u",random_number);
+    cout << endl;
 }
